40ca31d2937871126b0cc353f8947495e1761681
vings1 <vingfat@gmail.com>
2025-09-06 17:29:30 +0100

    Routes and Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          {""}
+            <Link href={"/"}>Home</Link>
+            <Link href={"/About"}>About</Link>
+            <Link href={"/Contact"}>Contact</Link>
+        </div>
         {children}
       </body>
     </html>
