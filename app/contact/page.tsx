88e6bc816168fe5002689c0794142b13f5ae563b
vings1 <vingfat@gmail.com>
2025-09-08 00:58:03 +0100

    API routes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,22 @@
+"use client"
+import { useState } from "react";
+
+async function makePostRequest(){
+    const res = await fetch(`/api/hello`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name:"Pedro"}),
+    });
+
+    const data = await res.json();
+    return {data};
+}
+
 export default function Contact(){
-    return <h1>Contact</h1>;
+    const [message, setMessage] = useState("");
+    const onClick = async () => {
+        const {data} = await makePostRequest();
+        setMessage(data.message);
+    }
+    return <h1>Contact, {message} <button onClick={onClick}>CLICK HERE</button></h1>;
 }
---
+++ app/users/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingState() {
+    return <h1>Loading</h1>
+}
